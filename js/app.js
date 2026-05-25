6e4d71f1168719b085d176cde48db932665877e2
Guilherme <anderson.g7santos@gmail.com>
2026-05-25 17:15:05 -0300

    - correção de assests para aba de info - correção do webnav em (INDEX, AGENDA, CULTURAL ) - incremento do countdonw ao cronograma da tela inicial - Add on 'web nav' = cultural - aba info agora carrega css - sw agora carrega images/icon

--- js/app.js
+++ js/app.js
@@ -13,6 +13,26 @@ const statusUpdate = () => {
     console.log("Status: Offline - Navegando via Cache do PWA");
   }
 };
+// countdown para o evento
+
+  const dataDoEvento = new Date("2026-06-15T20:00:00").getTime();
+
+  const atualizador = setInterval(function() {
+    const agora = new Date().getTime();
+    const distancia = dataDoEvento - agora;
+    const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    document.getElementById("dias").innerText = dias.toString().padStart(2, '0');
+    document.getElementById("horas").innerText = horas.toString().padStart(2, '0');
+
+    if (distancia < 0) {
+      clearInterval(atualizador);
+      document.getElementById("contador").innerHTML = "O evento começou!";
+    }
+    
+  }, 1000);
+
 
 window.addEventListener('online', statusUpdate);
 window.addEventListener('offline', statusUpdate);
